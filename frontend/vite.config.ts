a27afb48af83affa692efd7716f9165b7a5199df
Nicola Ananda Dwi Ervantoro <nicolaanandadwiervantoro@Nicolas-MacBook-Pro.local>
2025-12-30 18:40:32 +0700

    add presentasi

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,6 +11,14 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        presentasi: path.resolve(__dirname, 'presentasi/index.html'),
+      },
+    },
+  },
   server: {
     host: '0.0.0.0',
     port: 7781,
