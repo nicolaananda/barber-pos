da86900289d8c127092cc89230f61b20527e373e
Nicola Ananda <nicola@nicola.id>
2026-01-06 22:41:31 +0700

    Update WA_DEVICE_ID in ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
         NODE_ENV: 'production',
         PORT: 3001,
         WA_GATEWAY_URL: 'https://gowa.nicola.id',
-        WA_DEVICE_ID: '99eae17f-63a1-4fee-b5fb-10cdd48dcacb',
+        WA_DEVICE_ID: 'bagus_tab',
         WA_GATEWAY_USER: 'admin',
         WA_GATEWAY_PASS: '@Nandha20',
         WA_WEBHOOK_SECRET: 'apiku'
